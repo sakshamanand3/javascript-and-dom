883053ac66a0a42275e20fbf799e8db87203e6b3
sakshamanand3 <59570902+sakshamanand3@users.noreply.github.com>
2020-03-10 17:22:59 -0400

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,17 @@
+let tag = document.getElementById("tag-line");
+let headings = document.querySelectorAll(".bg-main-content h2");
+let collect = `${tag.textContent}\n`;
+for(let heading of headings){
+    collect += `\t${heading.textContent}\n`; 
+}
+alert(collect);
+
+
+
+let bxs = document.querySelectorAll('.bg-main-content .box');
+let when_to_launch = bxs[12];
+let scoopedElements = when_to_launch.children;
+
+let collect1 = `${scoopedElements[0].innerText}\n\n${scoopedElements[1].innerText}\n\n${scoopedElements[2].innerText}`
+alert(collect1);
+
